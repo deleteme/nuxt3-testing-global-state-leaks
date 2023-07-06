1c3573ffa186b6a5b1e6630dc66c885390655870
Matt Garrett <mattga@gmail.com>
2023-07-06 14:51:30 -0700

    add link to source

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <Count />
+    <p>
+      Requests to this page will increase a <code>count</code> variable.
+      This is a reproduction of how you can share or leak state across requests.
+      Open the browser console, <a :href="route.fullPath">reload</a>, and view the original response to this html page.
+    </p>
+    <p>Source: <a href="https://github.com/deleteme/nuxt3-testing-global-state-leaks">https://github.com/deleteme/nuxt3-testing-global-state-leaks</a></p>
   </div>
 </template>
+<script setup>
+const route = useRoute();
+</script>
+
--- components/Count.vue
+++ components/Count.vue
@@ -1,14 +1,8 @@
 <template>
   <div>Count: {{ count }}</div>
-  <p>
-    Requests to this page will increase a <code>count</code> variable.
-    This is a reproduction of how you can share or leak state across requests.
-    Open the browser console, <a :href="route.fullPath">reload</a>, and view the original response to this html page.
-  </p>
 </template>
 <script setup>
 import { useLeakyCount } from "~/composables/useLeakyCount";
 const count = useLeakyCount();
-const route = useRoute();
 </script>
 
